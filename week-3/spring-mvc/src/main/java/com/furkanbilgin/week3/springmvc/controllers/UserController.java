2a605a7505142fbc1b7ad014336157a71dad6730
Furkan Bilgin <info@furkanbilgin.net>
2025-08-05 10:29:01 +0300

    feat: add registration to spring-mvc

--- week-3/spring-mvc/src/main/java/com/furkanbilgin/week3/springmvc/controllers/UserController.java
+++ week-3/spring-mvc/src/main/java/com/furkanbilgin/week3/springmvc/controllers/UserController.java
@@ -1,15 +1,35 @@
 package com.furkanbilgin.week3.springmvc.controllers;
 
+import com.furkanbilgin.week3.springmvc.models.UserRegisterForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping(value="/hello")
-    public String hello() {
-        return "hello";
+    private final List<UserRegisterForm> userRegisterForms = new ArrayList<>();
+
+    @GetMapping(value="/register")
+    public String getRegister(Model model) {
+        UserRegisterForm form = new UserRegisterForm();
+        model.addAttribute("form", form);
+        return "register";
+    }
+
+    @PostMapping(value="/register")
+    public String postRegister(@ModelAttribute("form") @Valid UserRegisterForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        userRegisterForms.add(form);
+        model.addAttribute("users", userRegisterForms);
+        return "register-success";
     }
 }
---
+++ week-3/spring-mvc/src/main/java/com/furkanbilgin/week3/springmvc/models/UserRegisterForm.java
@@ -0,0 +1,22 @@
+package com.furkanbilgin.week3.springmvc.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public @Data class UserRegisterForm {
+    @Size(min = 3, message = "Name must be at least 3 characters long")
+    private String name;
+
+    @Size(min = 3, message = "Surname must be at least 3 characters long")
+    private String surname;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Min(value = 20, message = "Age must be at least 20")
+    private int age;
+}
