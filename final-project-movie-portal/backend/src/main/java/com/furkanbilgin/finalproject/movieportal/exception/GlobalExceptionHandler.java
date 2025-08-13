5519b838d1e2ff0b32aaab7f6f1b325026626480
Furkan Bilgin <info@furkanbilgin.net>
2025-08-13 14:27:51 +0300

    feat(final/backend): log exceptions

--- final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/exception/GlobalExceptionHandler.java
+++ final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.furkanbilgin.finalproject.movieportal.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -32,6 +35,7 @@ public class GlobalExceptionHandler {
     if (responseStatusAnnotation != null) {
       status = responseStatusAnnotation.value();
     }
+    logger.error("Exception occurred: ", ex);
     return new ResponseEntity<>(error, status);
   }
 }
