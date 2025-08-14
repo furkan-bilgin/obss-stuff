78f068947571ae243b2eaf834c8eaf21df3ba0d0
Furkan Bilgin <info@furkanbilgin.net>
2025-08-14 23:14:32 +0300

    fix(final/backend): fix return type of findByEmail

--- final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/repository/UserRepository.java
+++ final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.furkanbilgin.finalproject.movieportal.repository;
 
-import com.furkanbilgin.finalproject.movieportal.dto.user.UserDTO;
 import com.furkanbilgin.finalproject.movieportal.model.user.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     Optional<User> findByUsername(String username);
 
-    Optional<UserDTO> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
