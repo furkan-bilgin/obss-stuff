831de3b407b0ea0354ce0b98e6cd652a2a9c4020
Furkan Bilgin <info@furkanbilgin.net>
2025-08-14 20:11:31 +0300

    feat(final/backend): add validation annotations for UserUpdateMeDTO

--- final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/dto/user/UserUpdateMeDTO.java
+++ final-project-movie-portal/backend/src/main/java/com/furkanbilgin/finalproject/movieportal/dto/user/UserUpdateMeDTO.java
@@ -1,11 +1,16 @@
 package com.furkanbilgin.finalproject.movieportal.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class UserUpdateMeDTO {
+  @Email(message = "Email should be valid")
   private String email;
+
+  @Pattern(regexp = "^$|.{8,20}", message = "Password must be between 8 and 20 characters")
   private String password;
 }
